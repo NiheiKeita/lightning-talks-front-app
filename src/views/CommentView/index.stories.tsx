09888553ec6da4500c5c8a5b41ca584e84dfe18f
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-04-04 19:05:54 +0900

    Lint修正

--- src/views/CommentView/index.stories.tsx
+++ src/views/CommentView/index.stories.tsx
@@ -1,6 +1,4 @@
-import { expect } from '@storybook/jest';
 import { Meta, StoryObj } from '@storybook/react';
-import { userEvent, within } from '@storybook/testing-library';
 import { CommentView } from '.';
 
 
@@ -14,7 +12,7 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Test: Story = {
-  play: async ({ canvasElement }) => {
+  play: async () => {
     // const button = await within(canvasElement).findByTestId('button');
     // expect(button).toBeInTheDocument();
     // expect(within(canvasElement).queryByTestId('countText')?.innerHTML).toBe("0");
